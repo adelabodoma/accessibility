84944fd22363edeeb1f8e85e999717f5fc3e15d8
Adel Sadek <adel.sadek@linkdev.com>
2019-12-19 12:00:23 +0200

    fix issues

--- accessibility.js
+++ accessibility.js
@@ -19,13 +19,30 @@ var UI = (function () {
    {
       fontIncrease: '.accessability__href--increase',
       fontDecrease: '.accessability__href--decrease',
+
+      accessability__link: '.accessability__link',
+      accessability__main: '.accessability__main',
+
+      accessability__underLine: '.accessability__href--underLine',
+      accessability__fontReadable: '.accessability__href--font-readable',
+
+      // HELPERS CSS CLASSES
+      underLineClass: 'ACC__underlineClass'
+
    };
 
+   var htmlTemplate = '<nav class="accessability"> <div class="accessability__link"> <svg class="accessability__link--icon"> <use xlink:href="sprite.svg#icon-accessibility"></use> </svg> </div> <div class="accessability__main"> <h2 class="accessability__title"> Accessibility Tools </h2> <ul class="accessability__items"> <li class="accessability__item"> <a href="#" class="accessability__href accessability__href--increase"> <svg class="accessability--icon"> <use xlink:href="sprite.svg#icon-zoom-in"></use> </svg> <span class="accessability--text">Increase Text</span> </a> </li> <li class="accessability__item"> <a href="#" class="accessability__href accessability__href--decrease"> <svg class="accessability--icon"> <use xlink:href="sprite.svg#icon-zoom-out"></use> </svg> <span class="accessability--text">Decrease Text</span> </a> </li> <li class="accessability__item"> <a href="#" class="accessability__href"> <svg class="accessability--icon"> <use xlink:href="sprite.svg#icon-contrast"></use> </svg> <span class="accessability--text">High Contrast</span> </a> </li> <li class="accessability__item"> <a href="#" class="accessability__href"> <svg class="accessability--icon"> <use xlink:href="sprite.svg#icon-eye"></use> </svg> <span class="accessability--text">Negative Contrast</span> </a> </li> <li class="accessability__item"> <a href="#" class="accessability__href"> <svg class="accessability--icon"> <use xlink:href="sprite.svg#icon-link"></use> </svg> <span class="accessability--text">Links Underline</span> </a> </li> <li class="accessability__item"> <a href="#" class="accessability__href"> <svg class="accessability--icon"> <use xlink:href="sprite.svg#icon-text-color"></use> </svg> <span class="accessability--text">Readable Font</span> </a> </li> <li class="accessability__item"> <a href="#" class="accessability__href"> <svg class="accessability--icon"> <use xlink:href="sprite.svg#icon-reload"></use> </svg> <span class="accessability--text">Reset</span> </a> </li> </ul> </div> </nav>';
+
+   var htmlRender = function (selector, cb) {
+      document.querySelector(selector).innerHTML = htmlTemplate;
+      cb();
+   }
 
    return {
       getDomStrings: function () {
          return DOMStrings;
-      }
+      },
+      htmlRender: htmlRender
    }
 
 })();
@@ -34,16 +51,16 @@ var UI = (function () {
 
    "use strict";
 
-   function Accessibility(options) {
+   function Accessibility(selector, options) {
 
-      return new Accessibility(options);
+      return new Accessibility(selector, options);
    }
 
    /**
     * init the function with passed params.
     * @public
     */
-   Accessibility.init = function (options) {
+   Accessibility.init = function (selector, options) {
 
       // HTML ELEMENT
       // var ele = document.querySelector(selector);
@@ -60,8 +77,10 @@ var UI = (function () {
       Accessibility.options = extendObject(Accessibility.options, options);
 
 
-      // Fire events
-      setupEventListener();
+
+
+      // Render HTML Template
+      // setupHtmlRender(selector);
    }
 
    // ADD VERSION 
@@ -81,6 +100,9 @@ var UI = (function () {
       wordSpacing: false,
       increaseCursor: false
    }
+
+   // GET DOM SELECTOR
+   var selector = UI.getDomStrings();
 
 
    /** Functionalities
@@ -106,25 +128,30 @@ var UI = (function () {
 
       // check if size is an array not single size
       if (typeof fontSize === 'object' && Array.isArray(fontSize)) {
-         /**
-          * increase the count if type increase else decrease
-          * @var {fontCount}
-          */
-         // type == "increase" ?  (fontCount < fontSize.length ? fontCount++ : fontCount) : fontCount--;
 
          /**
          * increase the index and the count if type increase else decrease
          * @var {fontIndex}
          */
-         // type == "increase" ? (fontIndex < fontSize.length ? fontIndex++ : fontIndex) : fontIndex--;
          type == "increase" ? fontIndex++ : fontIndex--;
 
-         // fontIndex < 0 ? fontIndex = 0 : fontIndex = fontIndex;
-         // fontCount < 0 ? fontCount = 0 : fontCount = fontCount;
-
+         /**
+          * check  if @counter more than the array itself
+          * 
+          */
          if (fontIndex > fontSize.length) {
+            /**
+             * if the index @counter more than the array length. 
+             * font index will be the array length minus one 
+             */
+
             fontIndex = fontSize.length - 1;
-         }else if (fontIndex < 0) {
+
+         } else if (fontIndex < 0) {
+            /**
+             * check if index counter less than 0 ex. -1 
+             * font index will be first index ex. 0
+             */
             fontIndex = 0;
          }
 
@@ -135,12 +162,37 @@ var UI = (function () {
 
 
       } else if (typeof fontSize === 'string') { // if the font size is just one size 
-         document.querySelector('html').style.fontSize = fontSize;
+         /**
+          * check if the user pass a one value so we have to ways
+          * @first type === increase
+          * otherwise the type will be decrease
+          */
+         if (type === 'increase') {
+            /**
+             * 
+             * @HTML render
+             */
+            document.querySelector('html').style.fontSize = fontSize;
+
+         } else {
+            /**
+             * 
+             * @HTML render
+             */
+            document.querySelector('html').style.fontSize = '100%';
+         }
       }
    }
 
-   var linkHighlight = function (style) {
-      document.querySelector('head').appendChild(style);
+   var linkUnderline = function (style) {
+      // var links  = Array.from(document.querySelectorAll('a'));
+      // for (var i = 0; i < links.length; i++) {
+      //    var element = links[i];
+      //    element.style.textDecoration = 'underline';
+
+      // }
+
+      document.querySelector('body').classList.toggle(selector.underLineClass);
    }
 
    var headingHighlight = function (style) {
@@ -148,6 +200,7 @@ var UI = (function () {
    }
 
    var readableFont = function () {
+      document.querySelector('body').classList.toggle('ACC__fontReadable');
 
    }
 
@@ -167,7 +220,7 @@ var UI = (function () {
 
    // =>  extend object by merge them 
    var extendObject = function (defaultOptions, options) {
-      for (const key in defaultOptions) {
+      for (var key in defaultOptions) {
          if (!options.hasOwnProperty(key)) {
             options[key] = defaultOptions[key];
          }
@@ -179,13 +232,35 @@ var UI = (function () {
 
    // SETUP EVENT LISTENER
    var setupEventListener = function () {
-      // GET DOM SELECTOR
-      var selector = UI.getDomStrings();
-
       // FONT RESIZE EVENT
       document.querySelector(selector.fontIncrease).addEventListener('click', fontResize.bind(null, Accessibility.options.fontSize, 'increase'));
       document.querySelector(selector.fontDecrease).addEventListener('click', fontResize.bind(null, Accessibility.options.fontSize, 'decrease'));
+
+      // LINK UNDERLINE
+      document.querySelector(selector.accessability__underLine).addEventListener('click', linkUnderline);
+
+      // FONT READABLE
+      document.querySelector(selector.accessability__fontReadable).addEventListener('click', readableFont);
+
+
+      // MENU TOGGLE FUNCTION
+      document.querySelector(selector.accessability__link).addEventListener('click', function () {
+         document.querySelector(selector.accessability__main).classList.toggle('rightPosition');
+         this.classList.toggle('rightPosition-link');
+      })
    }
+
+   // RENDER THE HTML TEMPLATE FOR ACCESSABILITY TOOL
+   var setupHtmlRender = function (selector) {
+      UI.htmlRender(selector, function () {
+         debugger
+         // Fire events
+         setupEventListener();
+      })
+   }
+
+   setupEventListener();
+
 
    // define the tool in global level at window object
    // so we can access the tool direct from window 
@@ -203,10 +278,6 @@ var UI = (function () {
 })(UI);
 
 
-document.querySelector('.accessability__link').addEventListener('click', function () {
-
-
-   document.querySelector('.accessability__main').classList.toggle('rightPosition');
-   document.querySelector('.accessability__link').classList.toggle('rightPosition-link');
-
+accessibility.init('#accessability', {
+   fontSize: '10rem'
 })
